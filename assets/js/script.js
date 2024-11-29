3c5f2d462f15d7f41939f65f4d90d17303e17a46
EDUARDA VICTORIA SANTIAGO CELESTINO <43620385823@info.local>
2024-11-29 20:11:19 -0300

    mexido dia 29/11

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ const sectionMenuResponsivo = document.getElementById("menu_responsivo") ;
 
 function abrirMenuResponsivo() {
     //EXIBINDO
-    sectionMenuResponsivo.global.display = "initial";
+    sectionMenuResponsivo.global.style = "initial";
 }
 
+function fecharMenuResponsivo() {
+    //Ocultando
+    sectionMenuResponsivo.global.display = "none";
+}
