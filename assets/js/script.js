ec42391d33ec001f7f9ea8cfae3ccd9a76d3e30b
EDUARDA VICTORIA SANTIAGO CELESTINO <43620385823@info.local>
2024-11-28 21:51:09 -0300

    feat:comecamos java script

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const botaoHamburguer = document.getElementById("nav_hamburguer") ;
+const sectionMenuResponsivo = document.getElementById("menu_responsivo") ;
+
+function abrirMenuResponsivo() {
+    //EXIBINDO
+    sectionMenuResponsivo.global.display = "initial";
+}
+
